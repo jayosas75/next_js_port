c705232dd9e612cb9994a9d6a3c7d6dfa7c31e50
jayosas75 <jayosas75@gmail.com>
2020-10-29 14:56:17 -0700

    init

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from React
+
+class Home extends Component {
+    render() {
+        return (
+            <>
+                <h1>Hello and welcome</h1>
+            </>
+        )
+    }
+}
+
+export default Home;
